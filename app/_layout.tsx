25e58cb4887e215242e0d4dd7bc631610f5ebde8
mjjstockman <mjjstockman@gmail.com>
2024-02-04 15:33:43 +0000

    Add expo dev tools

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,17 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import { useNavigationContainerRef, Slot } from 'expo-router';
+import { useReactNavigationDevTools } from '@dev-plugins/react-navigation';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -47,11 +53,13 @@ export default function RootLayout() {
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
+  const navRef = useNavigationContainerRef();
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        <Stack.Screen name='modal' options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
   );
